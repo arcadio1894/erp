2b44e8efe8dda3ed7d6df92473e42c02b48dd57f
arcadio1894 <joryes1894@gmail.com>
2025-08-21 18:25:09 -0500

    Cambios en el listado de materiales almacen

--- resources/views/material/indexv2.blade.php
+++ resources/views/material/indexv2.blade.php
@@ -155,7 +155,7 @@
                             </select>
                         </div>
 
-                        <div class="col-md-2">
+                        {{--<div class="col-md-2">
                             <label for="material_type">Tipo:</label>
                             <select id="material_type" name="material_type" class="form-control form-control-sm select2" style="width: 100%;">
                                 <option value="">TODOS</option>
@@ -169,7 +169,7 @@
                                 <option value="">TODOS</option>
 
                             </select>
-                        </div>
+                        </div>--}}
                         <div class="col-md-2">
                             <label for="rotation">Rotación:</label>
                             <select id="rotation" class="form-control form-control-sm select2" style="width: 100%;">
@@ -185,7 +185,7 @@
                     <br>
 
                     <div class="row">
-                        <div class="col-md-2">
+                        {{--<div class="col-md-2">
                             <label for="cedula">Cédula:</label>
                             <select id="cedula" name="cedula" class="form-control form-control-sm select2" style="width: 100%;">
                                 <option value="">TODOS</option>
@@ -202,7 +202,7 @@
                                     <option value="{{ $arrayCalidades[$i]['id'] }}">{{ $arrayCalidades[$i]['name'] }}</option>
                                 @endfor
                             </select>
-                        </div>
+                        </div>--}}
                         <div class="col-md-2">
                             <label for="marca">Marca:</label>
                             <select id="marca" name="marca" class="form-control form-control-sm select2" style="width: 100%;">
